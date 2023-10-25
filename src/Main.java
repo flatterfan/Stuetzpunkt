a8be16c8d3c0a6d5038cf92f62776250a51f59c0
Moritz Lenhard <123739316+flatterfan@users.noreply.github.com>
2023-10-25 20:57:07 +0200

    basic setup

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
+import Stuetzpunkt.Stuetzpunkt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Stuetzpunkt s = new Stuetzpunkt(7);
     }
 }
---
+++ src/Stuetzpunkt/Stuetzpunkt.java
@@ -0,0 +1,15 @@
+package Stuetzpunkt;
+
+public class Stuetzpunkt {
+    byte[][] table;
+
+    public Stuetzpunkt(int days) {
+        this.table = new byte[12][days * 24]; // initialize table with 12 soldiers
+    }
+
+    public boolean isValidTable() {
+        return false;
+    }
+
+
+}
